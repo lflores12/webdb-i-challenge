f481c9fa430d6e9d1d64cb2c2a61940baa78b561
Leonel <leonelaf12@gmail.com>
2019-06-03 16:39:59 -0400

    made get request to /

--- server.js
+++ server.js
@@ -1,7 +1,19 @@
 const express = require('express');
 
+
 const server = express();
 
 // your code here
+
+
+server.use(express.json());
+
+server.get('/', (req, res ) => {
+    res.send(
+        "<h1>Accounts Database</h1>"
+    )
+});
+
+
 
 module.exports = server;
